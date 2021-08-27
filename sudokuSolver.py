a0e116ffb1526dca9311a220df7970f4a779d5e8
Dan Tran <dan2001tran@gmail.com>
2021-08-27 18:12:06 -0400

    Was able to implement the backtracking algorithm

--- sudokuSolver.py
+++ sudokuSolver.py
@@ -11,6 +11,11 @@ board = [
 
 #outputs the final board to the user
 def showBoard(board):
+
+    for k in range(12):
+         print('-', end=" ")
+    print()
+    
     for i in range(len(board)):
         #borders
         if i % 3 == 0 and i > 0:
@@ -27,6 +32,10 @@ def showBoard(board):
 
             else:
                 print(board[i][j], end =' ')
+
+    for k in range(12):
+         print('-', end=" ")
+    print()
             
 def isValid(board, num, row, col):
     #checking to see if the number chosen is already in the row
@@ -50,10 +59,37 @@ def isValid(board, num, row, col):
 
     return True
 
+def solve(board):
+    #base case where the board no longer has empty spaces
+    solved = True
+    for i in range(len(board)):
+        for j in range(len(board)):
+            if board[i][j] == 0:
+                solved = False
+                
+    if(solved):
+        return True
+    
+    #looks for first free space
+    for i in range(len(board)):
+        for j in range(len(board)):
+            #free space is found
+            if (board[i][j] == 0):
+                #tries to enter a number 
+                for num in range(1,10):
+                    if isValid(board, num, i, j):
+                        board[i][j] = num
+                        if solve (board):
+                             return True
+                        board[i][j] = 0
+                return False       
+                    
 
 
 def main():
     showBoard(board)
-    print(isValid(board, 5, 0, 3))
+    solve(board)
+    print("this is the answer")
+    showBoard(board)
 
 main()
